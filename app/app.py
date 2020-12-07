edd2d5e7ecdbdfc8cd85cd65b716e329861f3179
DavidDoppenberg <david.doppenberg@ortec.com>
2020-12-07 10:57:19 +0100

    variables

--- app/app.py
+++ app/app.py
@@ -1,1 +1,31 @@
+import os
+import sys
+import argparse
+
+
+def error(text):
+    '''Helper method to raise consistent errors.'''
+    raise RuntimeError(f"Invalid config: {text} Please check your .env file.")
+
+def read_env(key, optional=False):
+    ''''Read environment variable. Raises error if not optional and not found.'''
+    value = os.getenv(key)
+    if value:
+        value = value.strip()
+    if not value: 
+        value = None
+    if not optional and value is None:
+        error(f"Missing key '{key}'.")
+    return value
+
+# CLI 
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", required=False)
+args = parser.parse_args()
+
+# ENV
+test_var = read_env("TEST_VAR", optional=True)
+
 print("Hello!")
+print("CLI variable:", args.port)
+print("ENV variable:", test_var)
